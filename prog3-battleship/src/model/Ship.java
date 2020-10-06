e1c882b9dbe4bcbd2743c1fa6c0e0befc5a57833
dani <dani@DaniD>
2020-10-06 17:20:41 +0200

    getters,setters,relaciones,getshapeindex y a medias getabsolue positions

--- prog3-battleship/src/model/Ship.java
+++ prog3-battleship/src/model/Ship.java
@@ -4,28 +4,34 @@ import java.util.*;
 
 public class Ship {
 	
-	private static int BOUNDING_SQUARE_SIZE;
-	private static int HIT_VALUE;
-	private static int CRAFT_VALUE;
+	private static int BOUNDING_SQUARE_SIZE=5;
+	private static int HIT_VALUE=-1;
+	private static int CRAFT_VALUE=1;
 	private static char symbol;
 	private static String name;
 	private static int[][] shape;
+	private Coordinate position=null;
+	private Orientation orientation;
 	
-	public Ship(Orientation o,char s,String n)
+	public Ship(Orientation o,char s,String n)	// orientation N/E/S/W - name = nombre del barco - symbol = carácter representativo del barco - position = coords
 	{
+		orientation=o;
+		symbol = s;
+		name = n;
 		
 	}
 	
-	public Coordinate getPosition()
-	{
-		return null;
+	public Coordinate getPosition() { return new Coordinate(position);
 	}
-	
-	public void setPosition(Coordinate position)
-	{
-		
+
+
+
+	public void setPosition(Coordinate position) {
+		this.position = position;
 	}
-	
+
+
+
 	public String getName() { return null; }
 	
 	public Orientation getOrientation( ) { return null; }
@@ -34,21 +40,39 @@ public class Ship {
 	
 	public int getShapeIndex(Coordinate c)
 	{
-		return 1;
+		int x=c.get(0);
+		int y=c.get(1);
+		
+		return y*BOUNDING_SQUARE_SIZE+x;
 	}
 	
 	public Set<Coordinate> getAbsolutePositions(Coordinate c) 
 	{
+		for()
+		
 		return null;
 	}
 	
 	public Set<Coordinate> getAbsolutePositions() 
 	{
+		
 		return null;
 	}
 	
+	public static void setSymbol(char symbol) {
+		Ship.symbol = symbol;
+	}
+	
+	public void setOrientation(Orientation orientation) {
+		this.orientation = orientation;
+	}
+
 	public boolean hit(Coordinate c)
 	{
+		boolean alcanzado=false;
+		
+		if(c.equals(position)) 
+		
 		return false;
 	}
 	
